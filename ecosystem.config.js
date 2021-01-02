79b2cdcf39e6289e4dde5dc570eb96b46387b61c
bbaktaeho <lth9036@gmail.com>
2021-01-02 20:22:29 +0900

    Init: pm2 클러스터 모드 설정

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,8 +5,8 @@ module.exports = {
       script: "./dist/server.js",
       instances: 0,
       exec_mode: "cluster",
-      wait_ready: true,
-      kill_timeout: 5000, // SIGINT -> SIGKILL 전송까지의 시간
+      // wait_ready: true,
+      // kill_timeout: 5000, // SIGINT -> SIGKILL 전송까지의 시간
     },
   ],
 };
