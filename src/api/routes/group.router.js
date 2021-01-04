6e2752b46b48638e32c7ac14b5452c1ee182251e
bbaktaeho <lth9036@gmail.com>
2021-01-04 23:28:44 +0900

    Init

--- src/api/routes/group.router.js
+++ src/api/routes/group.router.js
@@ -6,7 +6,7 @@ function groupRouter(app) {
 
   router.post("/", (req, res) => {});
   router.get("/:groupId/members", (req, res) => {});
-  router.get("/:groupId/ranking", (req, res) => {});
+  router.get("/:groupId/statics", (req, res) => {});
   router.post("/:groupId/album", (req, res) => {});
   router.get("/:groupId/timeline", (req, res) => {});
 }
