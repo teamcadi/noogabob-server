e6faab7268f7726a091891c10c15adafa550d074
bn-tw2020 <skaxodn97@gmail.com>
2021-01-07 17:32:09 +0900

    Add: Key발급 및 데이터베이스 저장

--- src/api/controllers/group.controller.js
+++ src/api/controllers/group.controller.js
@@ -1,3 +1,17 @@
-const GroupController = {};
+import { getApi } from "../../utils/response";
+import GroupService from "../services/group.service";
+
+
+const GroupController = {
+    getKey: async (req, res, next) => {
+        try {
+          const key = await GroupService.getKey();
+          res.status(201).json(getApi(true, key));
+        } catch (e) {
+          // error handling
+          next(e);
+        }
+      },
+};
 
 export default GroupController;
--- src/api/services/group.service.js
+++ src/api/services/group.service.js
@@ -1,3 +1,13 @@
-const GroupService = {};
+import {v4 as uuidv4} from "uuid";
+import Family from "../models/family.model";
+
+const GroupService = {
+    getKey: async ()=> {
+      const key = uuidv4();
+      await Family.postKey(key);
+      return key;
+    },
+};
+GroupService.getKey();
 
 export default GroupService;
