24bbb4c7daa21ee1a75261c801c83339e1e83c87
bbaktaeho <lth9036@gmail.com>
2021-02-21 11:28:15 +0900

    [FIX] 인증 미들웨어 적용

--- src/api/routes/dog.router.js
+++ src/api/routes/dog.router.js
@@ -1,16 +1,17 @@
 import { Router } from "express";
 import DogController from "../controllers/dog.controller";
-import authorization from "../middlewares/auth/key.authorization";
+import auth from "../middlewares/auth/key.authorization";
 import validation from "../middlewares/validators/dog/dog.validation";
 const router = Router();
 
 function dogRouter(app) {
   app.use("/dogs", router);
 
-  router.get("/", authorization, DogController.getDog);
-  router.post("/:dogId/meals", authorization, validation.eatValidation, DogController.feedDog);
-  router.post("/:dogId/snacks", authorization, validation.eatValidation, DogController.snackDog);
-  router.put("/:dogId", authorization, validation.dogValidation, DogController.updateDog);
+  router.use(auth.authorization);
+  router.get("/", DogController.getDog);
+  router.post("/:dogId/meals", validation.eatValidation, DogController.feedDog);
+  router.post("/:dogId/snacks", validation.eatValidation, DogController.snackDog);
+  router.put("/:dogId", validation.dogValidation, DogController.updateDog);
 }
 
 export default dogRouter;
--- src/api/routes/user.router.js
+++ src/api/routes/user.router.js
@@ -1,16 +1,16 @@
 import { Router } from "express";
 import UserController from "../controllers/user.controller";
 import validates from "../middlewares/validators/user/user.validation";
-import authorization from "../middlewares/auth/key.authorization";
+import auth from "../middlewares/auth/key.authorization";
 const router = Router();
 
 function userRouter(app) {
   app.use("/users", router);
-
-  router.post("/", authorization, validates.userValidation, UserController.postUser);
-  router.get("/:userId", authorization, UserController.getUser);
-  router.put("/:userId", authorization, validates.userValidation, UserController.updateUser);
-  router.delete("/:userId", authorization, UserController.deleteUser);
+  router.use(auth.authorization);
+  router.post("/", validates.userValidation, UserController.postUser);
+  router.get("/:userId", UserController.getUser);
+  router.put("/:userId", validates.userValidation, UserController.updateUser);
+  router.delete("/:userId", UserController.deleteUser);
 }
 
 export default userRouter;
